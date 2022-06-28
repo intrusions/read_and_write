06ba940c7ba1cddd22fde40cce8598e761b37faa
xel <65361679+intrusions@users.noreply.github.com>
2022-06-28 17:02:22 +0200

    simple update

--- factice_prog/factice_prog.cpp
+++ factice_prog/factice_prog.cpp
@@ -8,14 +8,14 @@
 using namespace std;
 
 int main() {
-	int varInt(123456);
-	string varString("A simple string");
+	int var_int(123456);
+	string var_string("A simple string");
 
 	do {
 		cout << "Process ID: " << dec << GetCurrentProcessId() << endl;
 		cout << endl;
-		cout << "varInt       (0x" << hex << uppercase << (uintptr_t)&varInt << ") = " << dec << varInt << endl;
-		cout << "varString    (0x" << hex << uppercase << (uintptr_t)&varString << ") = " << varString << endl;
+		cout << "varInt       (0x" << hex << uppercase << (uintptr_t)&var_int << ") = " << dec << var_int << endl;
+		cout << "varString    (0x" << hex << uppercase << (uintptr_t)&var_int << ") = " << var_int << endl;
 		cout << endl;
 		cout << "Press ENTER to print again." << endl;
 		getchar();
