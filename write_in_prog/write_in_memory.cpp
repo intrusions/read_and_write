ac85318c4bf110275994d11a244694a62b6011b5
xel <65361679+intrusions@users.noreply.github.com>
2022-06-28 16:57:43 +0200

    simple update

--- write_in_prog/write_in_memory.cpp
+++ write_in_prog/write_in_memory.cpp
@@ -5,25 +5,36 @@ using namespace std;
 
 int main()
 {
-    HANDLE hProcess = OpenProcess(PROCESS_ALL_ACCESS, FALSE, 10008);
-        if (hProcess == NULL)
-        {
-            cout << "Erreur : " << GetLastError() << endl;
-            system("pause");
-            return EXIT_FAILURE;
-        }
+    HANDLE hProcess = OpenProcess(PROCESS_ALL_ACCESS, FALSE, 2472);
+    if (!hProcess)
+    {
+        cout << "Error : " << GetLastError() << endl;
+        system("pause");
+        return EXIT_FAILURE;
+    }
 
-        int intToWrite = 666;
-        WriteProcessMemory(hProcess, (LPVOID)0x8FFB50, &intToWrite, sizeof(int), NULL);
-        if (WriteProcessMemory != 0)
-            cout << "Overwritten successfully" << endl;
-        else
-        { 
-            cout << "error code : " << GetLastError() << endl;
-            system("pause");
-            return EXIT_FAILURE;
-        }
-        CloseHandle(hProcess);
-        cout << "Press ENTER to quit." << endl;
-        system("pause > nul");
+    int int_to_write = 666;
+    WriteProcessMemory(hProcess, (LPVOID)0xC7DEBCF604, &int_to_write, sizeof(int), NULL);
+    if (WriteProcessMemory)
+        cout << "int overwritten successfully" << endl;
+    else
+    {
+        cout << "Error code : " << GetLastError() << endl;
+        system("pause");
+        return EXIT_FAILURE;
+    }
+
+    string string_to_write = "xel was here";
+    WriteProcessMemory(hProcess, (LPVOID)0xC7DEBCF628, &string_to_write, sizeof(string), NULL);
+    if (WriteProcessMemory)
+        cout << "string overwritten successfully" << endl;
+    else
+    {
+        cout << "Error code : " << GetLastError() << endl;
+        system("pause");
+        return EXIT_FAILURE;
+    }
+    CloseHandle(hProcess);
+    cout << "Press ENTER to quit." << endl;
+    system("pause > nul");
 }
